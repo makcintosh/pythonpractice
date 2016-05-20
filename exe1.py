a97fa8f186ccc5525a413bb1d806fefcacdb9ac6
makcintosh <mark.mendoza@esutek.com>
2016-05-20 11:05:23 +0800

    Updated

--- exe1.py
+++ exe1.py
@@ -1,8 +1,15 @@
 counter = 100	#an integer assignement
 miles = 1000.0 #a floting point
 name = "John"	#a string
+age = input("enter age: ")
+a=10
+b=20
+
+
 
 print counter
 print miles
 print name
+print age
 
+print a
